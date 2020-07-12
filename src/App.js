8763c8e3d88d7b7a86ade00aebecb7b649dda969
arush.singh@xpertdox.com <arush.singh@xpertdox.com>
2020-07-12 13:52:35 +0530

    ToDo app initilize for deployment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todo, setTodo] = useState([]);
+
+  const addTodo = text => {
+    const newTodos = [...todo, { text }];
+    setTodo(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todo];
+    newTodos[index].isCompleted = true;
+    setTodo(newTodos);
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="todoList">
+        {todo.map((todo, index) => {
+          return(
+            <Todo
+              key={index}
+              index={index}
+              todo={todo}
+              completeTodo={completeTodo}
+            />
+          )
+        })}
+        <TodoForm addTodo={addTodo} />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+const Todo = ({ todo, index, completeTodo }) => {
+  return(
+    <div className='todo' style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
+      {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+      </div>
+    </div>
+  )
+}
+
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}

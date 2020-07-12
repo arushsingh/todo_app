339683be4144aedb01eb5fbea6b662d3f9cab395
arush.singh@xpertdox.com <arush.singh@xpertdox.com>
2020-07-12 14:44:39 +0530

    UI Updates for todo app

--- src/App.js
+++ src/App.js
@@ -2,49 +2,67 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  // empty state to render items when added
   const [todo, setTodo] = useState([]);
 
   const addTodo = text => {
-    const newTodos = [...todo, { text }];
+    // func to add items
+    const found = todo.some(el => el.text === text)
+    if (!found){
+      const newTodos = [...todo, { text }];
+      setTodo(newTodos);
+    } else {
+      alert('Already Exist !')
+    }
+  };
+
+  const removeTodo = index => {
+    // func to remove items
+    const newTodos = [...todo];
+    newTodos.splice(index, 1);
     setTodo(newTodos);
   };
 
-  const completeTodo = index => {
-    const newTodos = [...todo];
-    newTodos[index].isCompleted = true;
-    setTodo(newTodos);
-  };
-  
   return (
-    <div className="App">
-      <div className="todoList">
-        {todo.map((todo, index) => {
-          return(
-            <Todo
-              key={index}
-              index={index}
-              todo={todo}
-              completeTodo={completeTodo}
-            />
-          )
-        })}
-        <TodoForm addTodo={addTodo} />
+    <section className="container">
+      <div className="heading">
+        <img alt='image_alternate' className="heading__img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/756881/laptop.svg" />
+        <h1 className="heading__title">To-Do List</h1>
       </div>
-    </div>
+      {/* todo form to add items */}
+      <TodoForm addTodo={addTodo} />
+      <div>
+        <ul className="toDoList">
+        {/* todo items to render */}
+          {todo && todo.map((todo, index) => {
+            return (
+              <Todo
+                key={index}
+                index={index}
+                todo={todo}
+                removeTodo={removeTodo}
+              />
+            )
+          })}
+        </ul>
+      </div>
+    </section>
   );
 }
 
 export default App;
 
 
-const Todo = ({ todo, index, completeTodo }) => {
+const Todo = ({ todo, index, removeTodo }) => {
   return(
-    <div className='todo' style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
-      {todo.text}
-      <div>
-        <button onClick={() => completeTodo(index)}>Complete</button>
+    <li>
+      <div className='todo'>
+        {todo.text}
+        <div>
+          <button className='button removebutton' onClick={() => removeTodo(index)}>Remove</button>
+        </div>
       </div>
-    </div>
+    </li>
   )
 }
 
@@ -59,13 +77,19 @@ const TodoForm = ({ addTodo }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        className="input"
-        value={value}
-        onChange={e => setValue(e.target.value)}
-      />
-    </form>
+    <form onSubmit={handleSubmit} className="form">
+        <div>
+          <label className="form__label" htmlFor="todo">~ Today I need to ~</label>
+          <input className="form__input"
+              type="text" 
+              id="todo" 
+              name="to-do"
+              size="30"
+              value={value}
+              onChange={e => setValue(e.target.value)}
+              required />
+          <button className="button"><span>Submit</span></button>
+        </div>
+      </form>
   );
 }
